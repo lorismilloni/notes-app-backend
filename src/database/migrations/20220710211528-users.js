be9958e7264067d73a2e011962250cb912086abb
lorismilloni <lorismsimon@protonmail.comr>
2022-07-10 20:05:29 -0300

    chore: create users seed

--- src/database/migrations/20220710211528-users.js
+++ src/database/migrations/20220710211528-users.js
@@ -19,6 +19,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       avatar: {
         type: Sequelize.STRING,
         allowNull: true,
---
+++ src/database/seeders/20220710224734-users.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert(
+    'Users',
+    [
+      {
+        name: 'Fernando',
+        lastname: 'Ribeiro',
+        email: 'flsr.dev@gmail.com',
+        password: 'password',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        name: 'Loris',
+        lastname: 'Simon',
+        email: 'lorismsimon@gmail.com',
+        password: 'password',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ],
+
+    {},
+  ),
+  down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
+};
